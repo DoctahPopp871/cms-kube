ecf68a0e085f0a2b7258e5e573ecb395f7f73855
Andrew Popp <apopp@mycancerdb.com>
2020-02-26 13:01:04 -0500

    Updating install script to allow positional arguments

--- src/helm/install-dash.sh
+++ src/helm/install-dash.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+VALFILE=${1}
+APPNAME=${2}
 
-cp $(pwd)/env-values/mcdb000-values.yaml $(pwd)/dashboard/values.yaml
-helm install --debug -n mcdb-000 ./dashboard
+# Check for Value line item
+if [ -z ${1} ];then
+  echo "Value file not provided as argument it should be your first arg"
+  exit 1
+else echo "Using the ${1} value file to deploy the app ... "
+fi
+
+if [ -z ${2} ];then
+  echo "App name not provided as argument it should be your second arg"
+  exit 1
+else echo "Using the ${2} helm app name to deploy the app ... "
+fi
+
+set -e
+cp $(pwd)/env-values/${1} $(pwd)/dashboard/values.yaml
+helm install --debug -n ${2} ./dashboard
 rm -rf $(pwd)/dashboard/values.yaml
 :
-
-
-## Provide value at script invocation to set values file
-## Values file shoould be env var populated by positional argument
