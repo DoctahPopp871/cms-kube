af1c0527d6a88022df683b805ba05eafaf56bc92
Andrew Popp <apopp@mycancerdb.com>
2020-02-26 12:48:30 -0500

    improved helm chart for app, adding mcdb000 values, first pass at improving install-dash.sh

--- src/helm/install-dash.sh
+++ src/helm/install-dash.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cp $(pwd)/env-values/demo-values.yaml $(pwd)/dashboard/values.yaml
-helm install --debug -n mcdb-demo ./dashboard
+cp $(pwd)/env-values/mcdb000-values.yaml $(pwd)/dashboard/values.yaml
+helm install --debug -n mcdb-000 ./dashboard
 rm -rf $(pwd)/dashboard/values.yaml
 :
+
+
+## Provide value at script invocation to set values file
+## Values file shoould be env var populated by positional argument
